9126328da447d2e717601083a4eedabce72fdab4
syaloni06 <syalonib@gmail.com>
2025-02-07 22:09:29 +0530

    Header component integrated

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import { SiEventbrite } from "react-icons/si";
+import { BsFillBellFill } from "react-icons/bs";
+import { MdAddCard } from "react-icons/md";
+import { IoPersonCircleSharp } from "react-icons/io5";
 const Header = () => {
   return (
     <>
     <header className="bg-white shadow-md py-3 px-6 flex justify-between items-center">
-      <Link className="text-xl font-semibold text-yellow-500">Event Manager</Link>
-      <nav className="flex items-center space-x-6">
-        <Link to="/createevent" className="text-gray-700 hover:text-gray-900">Create Event</Link>
-        <Link to="/signin" className="flex items-center space-x-2 text-gray-700 hover:text-gray-900">
-          <span>Login</span>
+      <Link className="text-3xl font-bold flex italic w-1/3"><SiEventbrite className="text-amber-400 text-4xl"/>ventify</Link>
+      <nav className="flex items-center space-x-6 justify-end w-1/2">
+      <BsFillBellFill className="text-2xl"/>
+        <Link to="/createevent" className="font-bold text-lg flex gap-1"><MdAddCard className="text-2xl self-center"/>Event</Link>
+        <Link to="/signin" className="flex items-center space-x-2 font-bold text-lg gap-1"><IoPersonCircleSharp className="text-2xl self-center"/> SignIn
         </Link>
       </nav>
     </header>
